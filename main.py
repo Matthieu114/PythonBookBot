f5085a5b9a716698555bfb0edd033219dc6b33e0
Matthieu114 <mdenis300@gmail.com>
2025-01-13 16:51:09 +0100

    add word count function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+def count_words(text : str) -> int :
+    if not isinstance(text,str):
+        raise TypeError("only string arguments are allowed")  
+    return len(text.split())
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        words_count = count_words(file_contents)
+
 main()
