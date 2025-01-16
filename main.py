18fdd6047fe483015f36c4448d68802d782f6615
Matthieu114 <mdenis300@gmail.com>
2025-01-16 23:19:52 +0100

    clean up code and only include alphabet chars in the report

--- main.py
+++ main.py
@@ -13,22 +13,31 @@ def count_characters(text : str) -> int:
     concat_text = ''.join(text.lower().split())
 
     for char in concat_text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
     # can also use the counter function from collections
     # print(Counter(f"{concat_text}\n"))
     return char_count
 
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words_count = count_words(file_contents)
-        char_count = count_characters(file_contents)
 
-        print(f"--- Begin report of books/frankenstein.txt ---\n")
-        print(f"{words_count} found in the document\n")
-        for char in char_count:
-            print(f"the '{char}' character was found {char_count[char]} times\n")
+def read_file(path):
+     with open(path) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    file_contents = read_file(book_path)
+    words_count = count_words(file_contents)
+    char_count = count_characters(file_contents)
+
+    print(f"--- Begin report of {book_path} ---\n")
+    print(f"{words_count} found in the document\n")
+    for char in char_count:
+        print(f"the '{char}' character was found {char_count[char]} times\n")
+    print("--- End report ---")
+
 main()
